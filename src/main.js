55ff66f09528f987c6b8c64d9d2f067daafa024f
Shilo <shilo.klein1@gmail.com>
2022-11-27 21:33:52 +0200

    config

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+
 import './assets/main.css'
+import {
+    focusDirective,
+    rainbowDirective,
+    clickOutsideDirective,
+  } from './directives'
+
 
 const app = createApp(App)
 
+app.directive('focus', focusDirective)
+app.directive('rainbow', rainbowDirective)
+app.directive('click-outside', clickOutsideDirective)
+
 app.use(router)
+app.use(store)
 
 app.mount('#app')
